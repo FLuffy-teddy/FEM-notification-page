7ac83582f402863d636c51be6203bed861c8a6ff
Tyler Nichols <nicholsty72@gmail.com>
2023-05-25 21:43:20 -0500

    smmall css tweak to the padding on the day sent and then add in the last few notifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,67 @@
-import Navbar from '../components/navbar';
-import Feed from '../components/feed';
+import Navbar from "../components/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="p-4 bg-white">
-      <Navbar/>
-      <Feed/>
+      <Navbar />
+      <div className="bg-slate-100 p-4 my-1 flex items-center">
+        <Image
+          className="p-2"
+          src="/avatar-mark-webber.webp"
+          alt={"User Image"}
+          width={50}
+          height={50}
+        />
+        <div className="flex items-center">
+          <h3 className="text-xl text-black">
+            Mark Webber{" "}
+            <span className="text-lg text-slate-400">
+              reacted to your recent post
+            </span>
+            <span className="text-lg text-slate-600">
+              {" "}
+              My first tournament today!
+            </span>
+          </h3>
+          <h4 className="text-lg text-slate-400">1m ago</h4>
+        </div>
+      </div>
+      <div className="bg-slate-100 p-4 my-1 flex items-center">
+        <Image
+          className="p-2"
+          src="/avatar-angela-gray.webp"
+          alt={"User Image"}
+          width={50}
+          height={50}
+        />
+        <div className="flex items-center">
+          <h3 className="text-xl text-black">
+            Angela Gray{" "}
+            <span className="text-lg text-slate-400">followed you</span>
+          </h3>
+          <h4 className="text-lg text-slate-400">5m ago</h4>
+        </div>
+      </div>
+      <div className="bg-slate-100 p-4 my-1 flex items-center">
+        <Image
+          className="p-2"
+          src="/avatar-jacob-thompson.webp"
+          alt={"User Image"}
+          width={50}
+          height={50}
+        />
+        <div className="flex items-center">
+          <h3 className="text-xl text-black">
+            Jacob Thompson{" "}
+            <span className="text-lg text-slate-400">
+              has joined your group
+            </span>
+            <span className="text-lg text-blue-600"> Chess Club</span>
+          </h3>
+          <h4 className="text-lg text-slate-400">1 day ago</h4>
+        </div>
+      </div>
     </main>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,14 @@
 export default function Navbar() {
   return (
     <div className="flex justify-between mb-4">
-      <div>
-        <h1 className="text-3xl text-black">
-          Notifications
-          <div className="bg-blue-600">
-            <h2 className="text-3xl text-white p-4">3</h2>
-          </div>
-        </h1>
+      <div className="flex items-center">
+        <h1 className="text-3xl text-black pr-2">Notifications</h1>
+        <div className="bg-blue-600 rounded-md">
+          <h2 className="text-xl text-white px-2">3</h2>
+        </div>
       </div>
       <div>
-        <button className="text-slate-300 text-lg">Mark all as read</button>
+        <button className="text-slate-500 text-lg">Mark all as read</button>
       </div>
     </div>
   );
