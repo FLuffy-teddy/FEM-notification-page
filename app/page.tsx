538855988b79deeb9b6f0423325f55045197e8e6
Ty <ty@malkincreative.com>
2023-06-07 13:37:54 -0500

    need to rework the seen/unseen prop to manipulate it in the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/navbar";
+import Navbar from "@/components/navbar";
 import Image from "next/image";
 import Usermessage from "@/components/usermessage";
 
 export default function Home() {
   return (
-    <main className="p-4 bg-white min-h-screen">
+    <main className="p-4 bg-white min-h-screen max-w-xl m-auto">
       <Navbar />
       <Usermessage
         imagePath="/avatar-mark-webber.webp"
@@ -12,19 +12,14 @@ export default function Home() {
         action="reacted to your recent post"
         post="My first tournament today!"
         time="1m ago"
+        seen={false}
       />
       <Usermessage
         imagePath="/avatar-angela-gray.webp"
         user="Angela Gray"
         action="followed you"
         time="5m ago"
-      />
-      <Usermessage
-        imagePath="/avatar-mark-webber.webp"
-        user="Mark Webber"
-        action="reacted to your recent post"
-        post="My first tournament today!"
-        time="1m ago"
+        seen={false}
       />
       <Usermessage
         imagePath="/avatar-jacob-thompson.webp"
@@ -33,26 +28,41 @@ export default function Home() {
         post="Chess Club"
         postClub={true}
         time="1 day ago"
+        seen={false}
       />
-      <div className="bg-slate-100 p-4 my-1 flex items-center">
-        <Image
-          className="p-2"
-          src="/avatar-jacob-thompson.webp"
-          alt={"User Image"}
-          width={50}
-          height={50}
-        />
-        <div className="flex items-center">
-          <h3 className="text-xl text-black">
-            Jacob Thompson{" "}
-            <span className="text-lg text-slate-400">
-              has joined your group
-            </span>
-            <span className="text-lg text-blue-600"> Chess Club</span>
-          </h3>
-          <h4 className="text-lg text-slate-400">1 day ago</h4>
-        </div>
-      </div>
+      <Usermessage
+        imagePath="/avatar-rizky-hasanuddin.webp"
+        user="Rizky Hasanuddin"
+        action="sent you a private message"
+        time="5 days ago"
+        comment="Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
+        seen={true}
+      />
+      <Usermessage
+        imagePath="/avatar-kimberly-smith.webp"
+        user="Kimberly Smith"
+        action="commented on your picture"
+        time="1 week ago"
+        commentImage="/image-chess.webp"
+        seen={true}
+      />
+      <Usermessage
+        imagePath="/avatar-nathan-peterson.webp"
+        user="Nathan Peterson"
+        action="reacted to your recent post"
+        post="5 end-game strategies to increase your win rate"
+        time="2 weeks ago"
+        seen={true}
+      />
+      <Usermessage
+        imagePath="/avatar-anna-kim.webp"
+        user="Ana Kim"
+        action="has left the group"
+        post="Chess Club"
+        postClub={true}
+        time="2 weeks ago"
+        seen={true}
+      />
     </main>
   );
 }
--- components/usermessage.tsx
+++ components/usermessage.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 interface AcceptedProps {
   imagePath: string;
@@ -24,35 +27,54 @@ export default function Usermessage({
   comment,
 }: AcceptedProps) {
   const postResponseColor = postClub ? "text-blue-500" : "text-slate-600";
+  const postSeenBG = seen ? "bg-white" : "bg-slate-100";
+
+  const [messageSeen, setMessageSeen] = useState(Boolean);
+
+  function updateSeen() {
+    seen: !messageSeen;
+  }
 
   return (
-    <div className="bg-slate-100 p-4 my-1 flex items-center">
-      <Image
-        className="p-2"
-        src={imagePath}
-        alt={"User Image"}
-        width={50}
-        height={50}
-      />
-      <div>
-        <div className="flex items-center">
-          <h3 className="text-xl text-black">
-            {user} <span className="text-lg text-slate-400">{action}</span>
-            <span className={`text-lg ${postResponseColor} `}>{post}</span>
-          </h3>
-          {commentImage ? (
-            <Image src={commentImage} alt="User Image" height={20} width={20} />
+    <>
+      <div className={`p-4 my-1 flex items-center ${postSeenBG} rounded-md`}>
+        <Image
+          className="p-2"
+          src={imagePath}
+          alt={"User Image"}
+          width={50}
+          height={50}
+        />
+        <div>
+          <div className="flex items-center">
+            <h3 className="text-xl text-black">
+              {user} <span className="text-lg text-slate-400">{action}</span>
+              {post ? (
+                <span className={`text-lg ${postResponseColor} `}> {post}</span>
+              ) : null}
+            </h3>
+            {!seen ? (
+              <button
+                onClick={updateSeen}
+                className="h-2 w-2 bg-red-500 rounded-full"
+              ></button>
+            ) : null}
+          </div>
+          <div>
+            <h4 className="text-lg text-slate-400">{time}</h4>
+          </div>
+          {comment ? (
+            <div className="border border-slate-400 text-slate-400 rounded-md p-4">
+              {comment}
+            </div>
           ) : null}
         </div>
-        <div>
-          <h4 className="text-lg text-slate-400">{time}</h4>
-        </div>
+        {commentImage ? (
+          <div>
+            <Image src={commentImage} alt="User Image" height={30} width={30} />
+          </div>
+        ) : null}
       </div>
-      {comment ? (
-        <div className="border-1 border-slate-500 rounded-md p-4">
-          {comment}
-        </div>
-      ) : null}
-    </div>
+    </>
   );
 }
